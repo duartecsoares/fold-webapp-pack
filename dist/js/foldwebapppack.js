1d6f04c85bc4a4ec7ec6d06731e27d965ba4bd6a
Duarte Soares <duartecsoares@me.com>
2016-04-28 00:16:05 +0100

    public and api dirs removed

---
+++ dist/js/foldwebapppack.js
@@ -0,0 +1,48 @@
+"use strict";
+
+requirejs.config({
+
+    baseUrl : "../public/scripts/",
+
+	paths : {
+
+        "jquery" : "../vendor/jquery/dist/jquery",
+        "backbone" : "../vendor/backbone/backbone",
+        "underscore" : "../vendor/underscore/underscore",
+        "colorThief" : "../vendor/color-thief-umd/src/color-thief",
+        "text" : "../vendor/requirejs-text/text",
+        "json" : "../vendor/requirejs-plugins/src/json"
+
+    },
+
+    shim: {
+
+        "backbone": {
+            deps: ["underscore", "jquery", "text", "json", "colorThief"],
+            exports: "Backbone"
+        },
+        "jquery" : {
+            exports : "$"
+        },
+        "underscore": {
+            exports: "_"
+        },
+        "json": {
+            deps: ["text"]
+        },
+
+        "colorThief": {
+            exports: "colorThief"
+        }
+    },
+
+    urlArgs: "bust=" + (new Date()).getTime()
+
+});
+
+require(["backbone", "controllers/app"], function(Backbone, appController){
+
+    Backbone.history.start({ pushState: true});
+    appController.boot();
+
+});
